42177c6fb28bb610d7cf0c6937d280096b027a3d
Eduard Gevorgyan <117510728+gevorgyaned@users.noreply.github.com>
2024-04-03 17:39:21 +0400

    Update supplies.h

--- supplies.h
+++ supplies.h
@@ -35,32 +35,4 @@ static int WINDOW_HEIGHT =  400;
 static int THREADS_COUNT =    4;
 static int ITERATIONS    =  100;
 
-void print_m256d(__m256 reg) {
-    double d[4];
-    _mm256_store_pd(d, reg);
-    printf("%lf %lf %lf %lf\n", d[0], d[1], d[2], d[3]);
-}
-
-void print_m256i(__m256 reg, FILE *file) {
-    long d[4];
-    _mm256_storeu_si256((__m256i_u *)d, reg);
-    fprintf(file, "%lu\n", d[0]);
-    fprintf(file, "%lu\n", d[1]);
-    fprintf(file, "%lu\n", d[2]);
-    fprintf(file, "%lu\n", d[3]);
-}
-
-void print_m256_binary(__m256 reg)
-{
-    long d[4];
-    _mm256_storeu_si256((__m256i_u *)d, reg);
-
-    for (int i = 0; i < 4; ++i) {
-        for (int j = LONG_BIT - 1; j >= 0; --j) {
-            printf("%d", (d[i] >> j) & 1 ? 1 : 0);
-        }
-        putchar('\n');
-    }
-}
-
 #endif /* SUPPLIES_H */
